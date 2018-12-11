4786ba896413ffa476aa0f5c41bc63a9bc22c661
Nicholas Humes <njhumes@gmail.com>
2018-12-11 14:26:24 -0700

    finished part 1

---
+++ homework_part_2/app.js
@@ -0,0 +1,25 @@
+/*  1.   Concatenation - 2 strings added together to equal a value
+        Interpolation - put variables in between our strings
+
+    2.  DRY - stands for Don't Repeat Yourself. As in don't write the same code twice
+    3.  Declaring a variable is the first time the variable is used in the program
+        therefore, we need to use const or let to define it.
+        We could assing a value to an existing variable if it has already been declared
+    4.  We use const when we know that the variable won't ever change. We use let to define any variable that could change later on
+    5.  The parent directory is the folder that contains or hosts the files/directories within itself
+
+
+*/
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a != b);
+console.log(c >= d);
+console.log('Name' === 'Name');
+console.log(a + b == c);
+console.log(a + a < d);
+console.log(e === 'Kevin');
+console.log(48 == '48');
